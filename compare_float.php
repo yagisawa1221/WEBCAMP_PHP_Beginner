0feb79d7b7c24157430167d6941d17bfaf0c7d22
yagisawa1221 <yagisawa1221@example.com>
2023-03-28 07:47:15 +0000

    logical_operators.phpを書いた

--- compare_float.php
+++ compare_float.php
@@ -4,25 +4,25 @@ echo "普通に比較\n";
 $f = 0.3;
 var_dump($f === 0.3);
 
-// 
+//
 echo "\n";
 echo "0.1+0.2と0.3を比較\n";
 $f = 0.1 + 0.2;
 var_dump($f === 0.3);
 
-// 
+//
 echo "\n";
 echo "0.1+0.2と0.3をprintfで表示\n";
 printf("0.1+0.2: %f \n", $f);
 printf("    0.3: %f \n", 0.3);
 
-// 
+//
 echo "\n";
 echo "0.1+0.2と0.3を高精度のprintfで表示\n";
 printf("0.1+0.2: %.32f \n",$f);
 printf("    0.3: %.32f \n",0.3);
 
-// 
+//
 echo "\n";
 echo "0.1+0.2と0.3の現実的な比較\n";
 define('FLOAT_PRECISION',0.000001);
---
+++ logical_operators.php
@@ -0,0 +1,19 @@
+<?php
+
+//
+echo "論理演算しの確認\n";
+var_dump( true && false);
+var_dump( true || false);
+
+//
+$age = 20;
+$point = 499;
+
+//
+echo "\n";
+echo "「かつ」の場合\n";
+var_dump( ($age>= 20)&&($point>=500));
+
+echo "\n";
+echo "「または」の場合\n";
+var_dump( ($age >= 20)||($point >=500));
