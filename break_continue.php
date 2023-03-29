9ba5c01aaae819b5e827172255987be96606328f
yagisawa1221 <yagisawa1221@example.com>
2023-03-29 05:39:11 +0000

    break_continue.phpを書いた

---
+++ break_continue.php
@@ -0,0 +1,23 @@
+<?php
+//
+echo "\n continueの確認\n";
+for($i = 0; $i <10; ++$i){
+    echo "{$i}は奇数かな？\n";
+    if( ($i % 2) === 1){
+        continue;
+    }
+    echo "偶数でした！！\n";
+}
+
+//
+echo "\n breakの確認 \n";
+$i = 0;
+while($i < 10){
+    echo "{$i} \n";
+    //
+    if($i >= 5){
+        echo "{$i}なのでループを抜けます\n";
+        break;
+    }
+    ++$i;
+}
--- do_while.php
+++ do_while.php
@@ -1,10 +1,10 @@
 <?php
-// 
+//
 while(false){
     echo "whileここは動く？\n";
 }
 
-// 
+//
 do{
     echo "do-whileここは動く？\n";
 }while(false);
