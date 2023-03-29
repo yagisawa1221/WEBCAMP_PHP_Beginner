99b4a1eeb5cd801e52a961f4316bd49095f5fdaf
yagisawa1221 <yagisawa1221@example.com>
2023-03-29 08:48:44 +0000

    declare_random_int.phpを書いた

--- math.php
+++ math.php
@@ -4,11 +4,11 @@ $i = -100;
 $j =abs($i);
 echo "{$i} の絶対値は{$j} \n";
 
-// 
+//
 $f = 3.5;
-// 
+//
 $f_floor = floor($f);
 echo "{$f}の端数を切り捨てると{$f_floor} \n";
-// 
+//
 $f_ceil = ceil($f);
 echo "{$f}の端数を切り上げると {$f_ceil}\n";
