64c88a1e61075ec02f2ec8d300b2e11c7ef672ba
yagisawa1221 <yagisawa1221@example.com>
2023-03-30 03:16:19 +0000

    my_function.phpを書いた

---
+++ my_function.php
@@ -0,0 +1,26 @@
+<?php
+//
+function numToDouble(int $i):int
+{
+    //
+    $ret = $i * 2;
+
+    //
+    return $ret;
+}
+//
+$num = numToDouble(10);
+echo "10を2倍にしたら {$num} になった\n";
+
+// 
+function numAdd(int $a, int $b):int
+{
+    // 
+    $ret = $a + $b;
+    
+    // 
+    return $ret;
+}
+// 
+$add = numAdd(5,6);
+echo "5と6を足したら {$add} になった\n";
