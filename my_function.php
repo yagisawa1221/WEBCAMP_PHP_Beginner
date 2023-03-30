d77cbe0e1c5f1b7231a090ae4948e4b848c6d43b
yagisawa1221 <yagisawa1221@example.com>
2023-03-30 03:30:55 +0000

    variables_scope.phpを書いた

--- my_function.php
+++ my_function.php
@@ -12,15 +12,15 @@ function numToDouble(int $i):int
 $num = numToDouble(10);
 echo "10を2倍にしたら {$num} になった\n";
 
-// 
+//
 function numAdd(int $a, int $b):int
 {
-    // 
+    //
     $ret = $a + $b;
-    
-    // 
+
+    //
     return $ret;
 }
-// 
+//
 $add = numAdd(5,6);
 echo "5と6を足したら {$add} になった\n";
