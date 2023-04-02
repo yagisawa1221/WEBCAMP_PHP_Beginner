c5cc41364095fac80e2846135016c152000ffa0d
yagisawa1221 <yagisawa1221@example.com>
2023-04-02 12:12:05 +0000

    cookie.phpを書いた

--- server.php
+++ server.php
@@ -1,15 +1,15 @@
 <?php
-// 
+//
 function h(string $s): string
 {
     return htmlspecialchars($s , ENT_QUOTES);
 }
 
-// 
+//
 echo "ブラウザは" , h($_SERVER['HTTP_USER_AGENT']) ,"です。<br>\n";
 echo "接続元IPは" , h($_SERVER['REMOTE_ADDR']) , "です。<br>\n";
 echo "本当の接続元IPは" , h($_SERVER['HTTP_X_FORWARDED_FOR']) , "です。<br>\n";
 
-// 
+//
 $form_ip = $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'] ?? '';
 echo "接続元IPは" , h($form_ip) , "です。<br>\n";
