4a1b2b51e273d26ab0842b5255738fc80978fbcf
yagisawa1221 <yagisawa1221@example.com>
2023-04-02 11:34:47 +0000

    server.phpを書いた

--- form.php
+++ form.php
@@ -1,15 +1,16 @@
 <?php
 error_reporting(-1);
-ini_set('display_errors', 'on');
+int_set('display_errors' , 'on');
 
-// h()関数の定義
+// 
 function h(string $s) : string
 {
-    return htmlspecialchars($s, ENT_QUOTES);
+    return htmlspecialchars($s ,ENT_QUOTES);
 }
 
-// データの取得
+// 
 $input = $_GET['input_text'] ?? "";
 
-// 出力
-echo "あなたが入力したのは " , h($input) , " ですね";
+// 
+echo "あなたが入力したのは" , h($input) , "ですね";
+
--- form2.php
+++ form2.php
@@ -1,11 +1,11 @@
 <?php
-// 
+//
 function h(string $s) : string
 {
     return htmlspecialchars($s , ENT_QUOTES);
 }
 
-// 
+//
 $html_form_params = [
     "pw" => "パスワード",
     "radio_a" => "ラジオボタン",
@@ -15,12 +15,12 @@ $html_form_params = [
     "hide" => "隠れた入力(hidden)",
 ];
 
-// 
+//
 $datum = [];
-// 
+//
 foreach($html_form_params as $name => $type){
     $datum[$name] = $_POST[$name] ?? "";
 }
 
-// 
+//
 var_dump($datum);
