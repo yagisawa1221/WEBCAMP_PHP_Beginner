ddeb59b087029dfed332717578eecb35778e8ac9
yagisawa1221 <yagisawa1221@example.com>
2023-04-03 06:58:37 +0000

    session.phpを書いた

---
+++ session.php
@@ -0,0 +1,26 @@
+<?php
+// 
+ob_start();
+// 
+session_start();
+
+// 
+function h(string $s):string
+{
+    return htmlspecialchars($s, ENT_QUOTES);
+}
+
+// 
+$counter = $_SESSION['counter'] ?? 1;
+
+// 
+echo "ｯセッション）あなたがこのサイトを訪れたのは" , h(strval($counter)),"回目ですね。";
+
+// 
+$next_counter = $counter +1;
+
+// 
+$_SESSION['counter'] = $next_counter;
+
+// 
+ob_end_flush();
