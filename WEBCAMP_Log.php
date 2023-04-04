5bca8d12bbb4786cfc2b71cf6d96229b312c35ba
yagisawa1221 <yagisawa1221@example.com>
2023-04-04 09:46:33 +0000

    WEBCAMP_Log.php log_use.php log_use_2.phpを書いた

---
+++ WEBCAMP_Log.php
@@ -0,0 +1,12 @@
+<?php
+// 
+namespace WEBCAMP;
+
+// 
+class Log
+{
+    public function test()
+    {
+        echo "WEBCAMP内 Log内 test \n";
+    }
+}
---
+++ log_use.php
@@ -0,0 +1,7 @@
+<?php
+// ファイルの読み込み
+require_once( __DIR__ ."/WEBCAMP_Log.php");
+
+// 名前空間付きのクラスを使う
+$obj = new \WEBCAMP\Log();
+$obj->test();
